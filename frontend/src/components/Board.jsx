808db579f18b41a154b20e4f6f1e3b4d1f7ecfe4
Jaideep0802 <141119366+Jaideep0802@users.noreply.github.com>
2025-08-10 21:20:58 +0530

    serverless backend integration

--- frontend/src/components/Board.jsx
+++ frontend/src/components/Board.jsx
@@ -25,7 +25,7 @@ function Board() {
 
     const getBoard = async () => {
         try {
-            const response = await axios.post("https://sudoku-backend-dif5.onrender.com/solve", { board });
+            const response = await axios.post("https://sudoku-api-gold.vercel.app/api/solve", { board });
             console.log("API Response:", response.data.board);
               if (!response.data || !Array.isArray(response.data.board)) {
                     throw new Error("Invalid board data from API");
